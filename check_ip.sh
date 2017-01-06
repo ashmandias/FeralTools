8b7be8fcfa23970ed4c887cc90806382e2d9513a
Ashmandias <ashmandias@gmail.com>
2017-01-06 10:27:07 -0500

    added color

--- check_ip.sh
+++ check_ip.sh
@@ -16,6 +16,10 @@ HOSTNAME=${HOSTNAME//[^a-z]/}
 HOST="${HOSTNAME}.feralhosting.com"
 #HOST="google.com"
 
+COLOR_IP="\x0303"
+COLOR_BAD="\x0307"
+COLOR_END="\x03"
+
 IP_PREFIX="185"
 #HOST_IP="$(nslookup "${HOST}" | grep "${IP_PREFIX}" | awk '{print $2}')"
 HOST_IP="$(host "${HOST}" | grep "has address" | awk '{print $4}')"
@@ -24,14 +28,14 @@ HOST_IPv6="$(host "${HOST}" | grep "has IPv6 address" | awk '{print $5}')"
 BAD_IP="$(host "fake.feralhosting.com" | grep "has address" | awk '{print $4}')"
 
 if [[ "${BAD_IP}" == "${HOST_IP}" ]]; then
-	echo -n "The host "${HOST} does not appear to exist""
+	echo -en "The host "${COLOR_BAD}${HOST}${COLOR_END} does not appear to exist!""
 	exit
 else
-	echo -n "The IP for ${HOST} is ${HOST_IP}"
+	echo -en "The IP for ${HOST} is ${COLOR_IP}${HOST_IP}${COLOR_END}"
 fi
 
 if [[ "${HOST_IPv6}x" != "x" ]]; then
-	echo -n "; The IPv6 address is ${HOST_IPv6}"
+	echo -en "; The IPv6 address is ${COLOR_IP}${HOST_IPv6}${COLOR_END}"
 fi
 
 
